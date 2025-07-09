abe756660c1f70e949f059e4a51cf7ba0d398b3f
Panchaljay001 <jay.anaxistech@gmail.com>
2025-07-09 14:20:58 +0530

    cc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,11 +23,14 @@ import {
   getMetadataByPage,
   BASE_URL,
 } from '../lib/api';
+import Loader from '@/components/Loader';
 
 export default function Home() {
   const [heroes, setHeroes] = useState([]);
   const [moments, setMoments] = useState([]);
   const [trendingDesigns, setTrendingDesigns] = useState([]);
+
+  const [loading, setLoading] = useState(true);
   const [bachatMahotsavImages, setBachatMahotsavImages] = useState<string[]>([]);
   type Metadata = {
     title: string;
@@ -39,7 +42,13 @@ export default function Home() {
   };
 
   const [metadata, setMetadata] = useState<Metadata | null>(null);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1200); // Show loader for 1.2 seconds
 
+    return () => clearTimeout(timer);
+  }, []);
   useEffect(() => {
     async function fetchData() {
       try {
@@ -67,7 +76,7 @@ export default function Home() {
   if (!BASE_URL) {
     throw new Error('Missing NEXT_PUBLIC_API_BASE_URL in .env.local');
   }
-
+  if (loading) return <Loader />;
   return (
     <>
       {metadata && (
